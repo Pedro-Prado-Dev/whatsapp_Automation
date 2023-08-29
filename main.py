a15fe91e4a20b94b7000ebf43faa0738ee47f06a
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2023-08-28 22:20:09 -0300

    starting automation

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+service = Service(ChromeDriverManager().install())
+nav = webdriver.Chrome(service=service)
+nav.get("https://web.whatsapp.com")
+
+
+time.sleep(50)
 
